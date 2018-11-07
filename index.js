6006d53595e718c8240748c763265614e2002a96
Jeffrey Jiang <anon0503@gmail.com>
2018-11-07 22:31:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ function multiply(a,b){
 function divide(a,b){
   return (a/b)
 }
+function inc(a){
+  return (a+1)
+}
+function dec(a){
+  return (a-1)
+}
+function makeInt(a){
+  parseInt(a, 10)
+}
